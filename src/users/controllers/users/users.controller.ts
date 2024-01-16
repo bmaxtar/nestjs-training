7ba45af4604fd1c872b620d79af0e2a4f5ac9fad
bmaxtar <bmaxtar@gmail.com>
2024-01-16 18:11:17 +0000

    user by id

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { CreateUserDto } from './../../dtos/CreateUser.dto';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller('users')
@@ -8,9 +9,15 @@ export class UsersController {
     return [{ username: 'maxtar', email: 'bmaxtar@gmail.com' }];
   }
 
-  @Post()
-  createUser(@Req() request: Request, @Res() response: Response) {
-    console.log(request.body);
-    response.send('Created');
+  @Post('create')
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
+  }
+
+  @Get(':id')
+  getUserById(@Req() request: Request, @Res() response: Response) {
+    console.log(request.params);
+    response.send('');
   }
 }
